9a39201283885ce731f887fc942a4e4ad1136264
Morgan Redfield <redfieldm@gmail.com>
2011-09-20 11:07:26 -0700

    remove debug print in qpMAC and update README

--- qpcsmaca_mac.py
+++ qpcsmaca_mac.py
@@ -289,7 +289,7 @@ class cs_mac(threading.Thread):
         for item in m.data:
             temp_list.append(10*math.log10(item) + self.k)
         fft_sum_db = sum(temp_list)/m.vlen
-        print fft_sum_db
+        #print fft_sum_db
         
         #do threshold comparisons
         ret_val = 0
